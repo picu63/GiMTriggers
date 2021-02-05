ead587261099f61c0aa2741f95e8ba99aab798b8
picu63 <48946087+picu63@users.noreply.github.com>
2021-02-05 23:30:02 +0100

    first commit

---
+++ Assets/Scripts/DoorScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    public GameObject door;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = door.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        animator.SetBool("isOpen", true);
+        animator.SetTrigger("trigger");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        animator.SetBool("isOpen", false);
+        animator.SetTrigger("trigger");
+    }
+}
